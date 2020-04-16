c5c5001a55462b3adf46c6359256fc3f21be1d35
dmitrithedwarf <dmitriventro@gmail.com>
2020-04-17 01:10:31 +0700

    ms_cmm_1

--- Stroki/Source.cpp
+++ Stroki/Source.cpp
@@ -1,6 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 #include <iostream>
+#include <stdlib.h>
+
+using namespace std;
+namespace copy_str
+{
+	void input(char *str1)
+	{
+		cout << "¬веди строку:\n";
+		fgets(str1, 10, stdin);
+		cout << str1;
+	}
+}
 int main()
 {
-	char* strcpy();
-	return 0;
+	setlocale(LC_ALL, "");
+	char *str1[10];
+	
 }
