ada23e65d286a93347ec1c9a7fff67e09bf17ffb
dmitrithedwarf <dmitriventro@gmail.com>
2020-04-27 17:43:30 +0700

    2_cmm_2

---
+++ Stroki/Source.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<fstream>
+using::std::cout;
+using::std::cin;
+using::std::endl;
+void input_file(int** arr_A, int** arr_C, int* vec_B, int* vec_D, int m, int n)
+{
+	std::string way = "D:\\who\\LoveCPP.txt";
+	std::ifstream file_in;
+	file_in.open(way);
+	
+	cout << std::endl;
+}
+
+int main()
+{
+
+}
