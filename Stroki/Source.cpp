a192b1dbb1c66abb34c28dcd5ba04617c2e38b8e
dmitrithedwarf <dmitriventro@gmail.com>
2020-04-27 18:11:10 +0700

    2_cmm_4

--- Stroki/Source.cpp
+++ Stroki/Source.cpp
@@ -2,29 +2,39 @@
 #include<fstream>
 #include<string.h>
 using namespace std;
-void input_file()
+char* input_file()
 {
+	int k, i;
+	k = 0;
+	char Str;
 	string str;
 	ifstream file;
 	file.open("D:\\who\\LoveCPP.txt", ios::in);
 	if (!file.is_open()) { cout << "Error: no file\n" << endl; }
 	else
 	{
-		while (file.get(s))
+		while (file.get(Str))
 		{
-			if (s == '!'  s == '?'  s == '.') break;
+			if (Str == '!'	|| Str == '?'	|| Str == '.') break;
 			else k++;
 		}
 	}
 	char* s2 = new char[k];
-	for (int i = 0; i < k; i++)
+	for (i = 0; i < k; i++)
 	{
 		file >> s2;
 		cout << s2 << "\t";
 	}
+	return s2;
 }
 
 int main()
 {
-
+	
+	char* fileStr = input_file();
+	setlocale(LC_ALL, "");
+	//char str1[10];
+	//char str2[10];
+	//int lenStr = copy_str::size_str1(str1);
+	//cout << "Скопированная строка:\n" << str2;
 }
