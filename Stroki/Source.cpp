e579d6fa29ad02cfbdda128eb08cff375df1b68e
dmitrithedwarf <dmitriventro@gmail.com>
2020-05-04 15:38:38 +0700

    ThirdTask_cmm_2

--- Stroki/Source.cpp
+++ Stroki/Source.cpp
@@ -1,9 +1,14 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define DELIM " \t\n,.:;?!"
 #include <iostream>
 #include <fstream> 
 #include <string>
 using namespace std;
-namespace SecondTask
+struct word_t {
+	char* _word;
+	size_t length;
+};
+namespace ThirdTask
 {
 	void FindTheShortestWord(char* MainStr, int min, char* BeforeChangesStr)
 	{
@@ -17,6 +22,13 @@ namespace SecondTask
 			}
 		}
 	}
+	
+
+	void _swap(word_t& w1, word_t& w2) {
+		word_t tmp = w1;
+		w1 = w2;
+		w2 = tmp;
+	}
 }
 
 int main()
@@ -26,7 +38,13 @@ int main()
 	char MainStr[100];
 	char BeforeChangesStr[100];
 	char AllFileInOneArray[3][41];
-	const char p = '\n';
+	char str1[100];
+	char str2[100];
+	char str3[100];
+	char *NewStr1;
+	char *NewStr2;
+	char *NewStr3;
+	char p = '\n';
 	string path = "D:\\who\\LoveCPP.txt";
 	ifstream fin;
 	fin.open(path);
@@ -41,8 +59,32 @@ int main()
 		cout << AllFileInOneArray[i] << "\n";
 	}
 	cout << endl;
-	strcpy(MainStr, AllFileInOneArray[0]);
-	SecondTask::FindTheShortestWord(MainStr, min, BeforeChangesStr);
-	cout << endl << BeforeChangesStr;
+	word_t words[BUFSIZ];
+
+
+	/*
+	strcpy(str1, AllFileInOneArray[0]);
+	strcpy(str2, AllFileInOneArray[1]);
+	strcpy(str3, AllFileInOneArray[2]);
+	cout << str1 <<"\n";
+	cout << str2 <<"\n";
+	cout << str3 <<"\n";
+	*/
+	int i = 0, j;
+	for (NewStr1 = strtok(str1, DELIM); NewStr1; NewStr1 = strtok(NULL, DELIM)) {
+		words[i]._word = new char[strlen(NewStr1) + 1];
+		strcpy(words[i]._word, NewStr1);
+		words[i++].length = strlen(NewStr1);
+	}
+	for (int i_ = 0; i_ < i; ++i_)
+		for (j = i_; j < i; ++j)
+			if (words[i_].length > words[j].length)
+				ThirdTask::_swap(words[i_], words[j]);
+	for (j = 0; j < i; ++j)
+		std::cout << words[j]._word << ' ';
+	for (j = 0; j < i; ++j)
+		delete[] words[j]._word;
+
+	cout << endl << AllFileInOneArray;
 	return 0;
 }
