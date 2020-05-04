b7dd64234cc6a1ab37006c79c6a0e4423a982c22
dmitrithedwarf <dmitriventro@gmail.com>
2020-05-04 14:31:14 +0700

    ThirdTask_cmm_2

---
+++ Stroki/Source.cpp
@@ -0,0 +1,48 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <fstream> 
+#include <string>
+using namespace std;
+namespace SecondTask
+{
+	void FindTheShortestWord(char* MainStr, int min, char* BeforeChangesStr)
+	{
+		cout << MainStr;
+		for (char* p = strtok(MainStr, " "); p; p = strtok(NULL, " "))
+		{
+			if (strlen(p) < min)
+			{
+				min = strlen(p);
+				strcpy(BeforeChangesStr, p);
+			}
+		}
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	int min = 25;
+	char MainStr[100];
+	char BeforeChangesStr[100];
+	char AllFileInOneArray[3][41];
+	const char p = '\n';
+	string path = "D:\\who\\LoveCPP.txt";
+	ifstream fin;
+	fin.open(path);
+	if (!fin.is_open())
+	{
+		cout << "файла не будет он принял ислам:/\n";
+		return 0;
+	}
+	for (int i = 0; i < 3; i++)
+	{
+		fin.getline(AllFileInOneArray[i], 41 - 1, p);
+		cout << AllFileInOneArray[i] << "\n";
+	}
+	cout << endl;
+	strcpy(MainStr, AllFileInOneArray[0]);
+	SecondTask::FindTheShortestWord(MainStr, min, BeforeChangesStr);
+	cout << endl << BeforeChangesStr;
+	return 0;
+}
