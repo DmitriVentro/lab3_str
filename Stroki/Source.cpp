06b34e482074088c93fd20fe8ed7ce99da6b6c92
dmitrithedwarf <dmitriventro@gmail.com>
2020-05-04 20:04:47 +0700

    amIdiot

--- Stroki/Source.cpp
+++ Stroki/Source.cpp
@@ -1,22 +1,2 @@
-#include <stdio.h>
-#include <string.h>
-#include <ctype.h>
-#include <iostream>
-#include <stdlib.h>
-
-using namespace std;
-namespace copy_str
-{
-	void input(char *str1)
-	{
-		cout << "Введи строку:\n";
-		fgets(str1, 10, stdin);
-		cout << str1;
-	}
-}
-int main()
-{
-	setlocale(LC_ALL, "");
-	char *str1[10];
-	
-}
+//Это шутка:)
+//Все проги раскиданы по веткам, смотрите!!1
